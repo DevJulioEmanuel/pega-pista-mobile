5ec2f8a650fd72f46143d53c2298ed2417f904d9
DevJulioEmanuel <julioemanuel100@gmail.com>
2025-11-18 23:07:34 -0300

    FeedScreen-versao1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation("androidx.compose.material:material-icons-extended:1.7.5") //JULIO: ICONS
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
