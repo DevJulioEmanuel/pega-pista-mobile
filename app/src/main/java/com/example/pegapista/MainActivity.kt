1cb61178e3f0c1e25ec6fed9965ab66cd3c3fcd8
Henriwi-ms <henrique.mendes2352@gmail.com>
2025-12-07 13:29:27 -0300

    feature: Notification Channel

--- app/src/main/java/com/example/pegapista/MainActivity.kt
+++ app/src/main/java/com/example/pegapista/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.pegapista
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +15,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNotificationChannel()
         setContent {
             PegaPistaTheme {
                 PegaPistaScreen()
             }
         }
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notificações PegaPista"
+            val descriptionText = "Canal para avisos de corridas e conquistas"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel("CHANNEL_ID_PEGAPISTA", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
