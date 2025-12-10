66ee3416f50c015e80709c3362e3efe6053cef64
DevJulioEmanuel <julioemanuel100@gmail.com>
2025-12-10 14:42:39 -0300

    commits

--- app/src/main/java/com/example/pegapista/MainActivity.kt
+++ app/src/main/java/com/example/pegapista/MainActivity.kt
@@ -19,9 +19,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // MUDANÇA AQUI:
-                    // Em vez de chamar NavigationGraph(), chamamos o PegaPistaScreen()
-                    // O PegaPistaScreen já tem o NavigationGraph dentro dele.
                     PegaPistaScreen()
                 }
             }
--- app/src/main/java/com/example/pegapista/ui/screens/FeedScreen.kt
+++ app/src/main/java/com/example/pegapista/ui/screens/FeedScreen.kt
@@ -1,6 +1,5 @@
-package com.example.pegapista.ui
+package com.example.pegapista.ui.screens
 
-import android.widget.Button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,7 +19,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.ModeComment
 import androidx.compose.material.icons.outlined.ModeComment
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
@@ -32,7 +30,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -68,7 +65,7 @@ fun FeedScreen(modifier: Modifier = Modifier.background(Color.White)) {
 
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
-                contentPadding = androidx.compose.foundation.layout.PaddingValues(16.dp),
+                contentPadding = PaddingValues(16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 items(postagens) { post ->
