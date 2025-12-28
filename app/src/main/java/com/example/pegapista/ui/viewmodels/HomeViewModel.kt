b2131fbd97539051b96995c7f9ac5369b8eb2fa0
Henriwi-ms <henrique.mendes2352@gmail.com>
2025-12-28 13:15:23 -0300

    feature: Ranking Funcional

--- app/src/main/java/com/example/pegapista/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/pegapista/ui/viewmodels/HomeViewModel.kt
@@ -15,6 +15,9 @@ class HomeViewModel : ViewModel() {
     private val _usuario = MutableStateFlow<Usuario?>(null)
     val usuario: StateFlow<Usuario?> = _usuario
 
+    private val _ranking = MutableStateFlow<List<Usuario>>(emptyList())
+    val ranking: StateFlow<List<Usuario>> = _ranking
+
     init {
         carregarDadosUsuario()
     }
@@ -24,6 +27,7 @@ class HomeViewModel : ViewModel() {
             try {
                 val user = userRepository.getUsuarioAtual()
                 _usuario.value = user
+                _ranking.value = userRepository.getRankingSeguindo()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
