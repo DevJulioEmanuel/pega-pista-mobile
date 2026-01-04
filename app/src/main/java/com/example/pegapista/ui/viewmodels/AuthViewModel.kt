f8beaf1c8f9fa98299575a18cdadf85b3e8496fe
DevJulioEmanuel <julioemanuel100@gmail.com>
2026-01-03 23:38:03 -0300

    fix e melhorias

--- app/src/main/java/com/example/pegapista/ui/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/example/pegapista/ui/viewmodels/AuthViewModel.kt
@@ -3,10 +3,13 @@ package com.example.pegapista.ui.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pegapista.data.repository.AuthRepository
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 // Estado da UI para autenticação
 data class AuthUiState(
@@ -68,5 +71,25 @@ class AuthViewModel : ViewModel() {
                 _uiState.value = AuthUiState(error = exception.message ?: "Erro ao cadastrar")
             }
         }
+    }
+
+    //GOOGLE
+
+    fun loginComGoogle(idToken: String) {
+        _uiState.value = AuthUiState(isLoading = true)
+
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        val auth = FirebaseAuth.getInstance()
+
+        auth.signInWithCredential(credential)
+            .addOnSuccessListener { result ->
+                _uiState.value = AuthUiState(isSuccess = true)
+            }
+            .addOnFailureListener { e ->
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    error = "Erro no Google: ${e.message}"
+                )
+            }
     }
 }
